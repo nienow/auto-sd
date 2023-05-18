2441c7af0b05c617628715fc18714d0c1f9090ae
nienow <system@randombits.dev>
2023-05-18 16:22:54 -0500

    Using weights in prompt generator

--- src/generator.ts
+++ src/generator.ts
@@ -6,23 +6,23 @@ import {ENV} from './env';
 import {generateSettings} from './settings-gen';
 
 export const generateImage = async () => {
-    const params = generateSettings();
-    console.log(params);
-    params.prompt = generatePrompt();
-    params.negative_prompt = generateNegative();
+  const params = generateSettings();
+  // const params = fastSettings as any;
+  params.prompt = generatePrompt();
+  params.negative_prompt = generateNegative();
 
-    const result = await fetch(`http://${ipAddress}:${CONFIG.port}/sdapi/v1/txt2img`, {
-        method: 'POST',
-        body: JSON.stringify(params),
-        headers: {
-            "Content-type": "application/json"
-        }
-    }).then(res => res.json())
+  const result = await fetch(`http://${ipAddress}:${CONFIG.port}/sdapi/v1/txt2img`, {
+    method: 'POST',
+    body: JSON.stringify(params),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  }).then(res => res.json());
 
-    const filename = new Date().getTime();
-    fs.writeFileSync(`${ENV.OUTPUT}/${filename}.png`, new Buffer(result.images[0], "base64"));
+  const filename = new Date().getTime();
+  fs.writeFileSync(`${ENV.OUTPUT}/${filename}.png`, new Buffer(result.images[0], 'base64'));
 
-    // fs.outputFileSync(`${path}/${filename}.txt`, JSON.stringify(params));
+  // fs.outputFileSync(`${path}/${filename}.txt`, JSON.stringify(params));
 
-    // fs.outputFileSync(newFile, html);
+  // fs.outputFileSync(newFile, html);
 };
--- src/prompt-gen.ts
+++ src/prompt-gen.ts
@@ -4,19 +4,38 @@ const TAGS = getTAGS();
 
 export const generatePrompt = () => {
   const randomPart = Object.keys(TAGS).map((key) => {
-      return randomProperty(TAGS[key])[0];
+    return randomValue(TAGS[key]);
   }).filter((str) => !!str).join(',');
-    console.log('Generating', randomPart);
+  console.log('Generating', randomPart);
 
-
-  return ENV.BASE_PROMPT + ',' + randomPart;
+  return randomPart;
 };
 
 export const generateNegative = () => {
   return ENV.BASE_NEG;
 };
 
+const randomValue = (obj) => {
+  while (true) {
+    const prop = randomProperty(obj);
+    const randomWeight = Math.random();
+    const weight = prop[2] || 1;
+    if (weight > randomWeight) {
+      return propToTag(prop);
+    }
+  }
+};
+
+const propToTag = (prop: any) => {
+  if (prop.length === 3 && prop[2] !== 1) {
+    return `(${prop[0]}: ${prop[2]})`;
+  } else {
+    return prop[0];
+  }
+};
+
 const randomProperty = (obj) => {
-    const keys = Object.keys(obj);
-    return obj[keys[ keys.length * Math.random() << 0]];
+  const keys = Object.keys(obj);
+  return obj[keys[keys.length * Math.random() << 0]];
+
 };
